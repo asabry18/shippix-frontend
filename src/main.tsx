e8316618094e8680b51ed7f0fb21d2491e4f1f2e
Ahmed Sabry <mohamedmahrous581@gmail.com>
2025-09-23 10:12:47 +0300

    feat: finish admin-panel page

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import LoginAdmin from './pages/loginAdmin/loginAdmin'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginAdmin />} />
+        <Route path="/admin-login" element={<LoginAdmin />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 )
